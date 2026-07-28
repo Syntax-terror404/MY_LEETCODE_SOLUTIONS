101a2f4fa06f011334093d9220d5339fae74b226
Rushikesh Amol Mule <rushikesh.m_2028@woxsen.edu.in>
2026-07-28 10:32:39 +0530

    Add solution for Group Anagrams

---
+++ solutions/medium/Group Anagrams.py
@@ -0,0 +1,17 @@
+// Title: Group Anagrams
+            // Difficulty: Medium
+            // Language: Python
+            // Link: https://leetcode.com/problems/group-anagrams/
+
+            key
+            sorted_word = "".join(sorted(word))
+            
+            # Sort the characters of the word to use as a 
+        for word in strs:
+        
+        res = defaultdict(list)
+    def groupAnagrams(self, strs):
+class Solution(object):
+            # Group original words under their sorted key
+            res[sorted_word].append(word)
+            
