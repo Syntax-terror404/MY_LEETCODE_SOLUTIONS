0b23f2c09f57c1fe3776cdf858a97a5070c13c7e
Rushikesh Amol Mule <rushikesh.m_2028@woxsen.edu.in>
2026-08-01 22:28:15 +0530

    Add solution for Single Number

---
+++ solutions/easy/Single Number.swift
@@ -0,0 +1,14 @@
+// Title: Single Number
+            // Difficulty: Easy
+            // Language: Swift
+            // Link: https://leetcode.com/problems/single-number/
+
+            if mySet.contains(num){
+                mySet.remove(num)
+            }else{
+        }    
+                mySet.insert(num)
+            }
+    }
+        return mySet.first!
+}
