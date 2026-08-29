140e5282a485450df19be1afc7339e19b8f4e4b6
Rushikesh Amol Mule <rushikesh.m_2028@woxsen.edu.in>
2026-08-29 23:26:17 +0530

    Add solution for Maximum Nesting Depth of the Parentheses

---
+++ solutions/easy/Maximum Nesting Depth of the Parentheses.py
@@ -0,0 +1,15 @@
+// Title: Maximum Nesting Depth of the Parentheses
+            // Difficulty: Easy
+            // Language: Python
+            // Link: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+
+            if char == "(":
+                cur_depth += 1
+                if cur_depth > max_depth:
+        for char in s:
+        
+                    max_depth = cur_depth 
+            elif char == ")":
+                cur_depth -= 1 
+                
+        return max_depth
