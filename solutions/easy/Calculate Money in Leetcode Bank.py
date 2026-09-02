ade638a713e5d091a40b5e53026087deaabf5664
Rushikesh Amol Mule <rushikesh.m_2028@woxsen.edu.in>
2026-09-02 19:00:38 +0530

    Add solution for Calculate Money in Leetcode Bank

---
+++ solutions/easy/Calculate Money in Leetcode Bank.py
@@ -0,0 +1,15 @@
+// Title: Calculate Money in Leetcode Bank
+            // Difficulty: Easy
+            // Language: Python
+            // Link: https://leetcode.com/problems/calculate-money-in-leetcode-bank/
+
+        for i in range(days):
+        start_date = week + 1
+            total += start_date + i 
+        return total 
+        total = 28 * week + 7 * ( week * (week - 1)) // 2 
+        days = n % 7
+        week = n / 7
+        
+            
+
