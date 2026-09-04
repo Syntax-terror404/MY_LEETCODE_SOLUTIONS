835f82192ac1b813a46c95bbf8ff5ea042f7c63a
Rushikesh Amol Mule <rushikesh.m_2028@woxsen.edu.in>
2026-09-04 12:01:44 +0530

    Add solution for Search a 2D Matrix

---
+++ solutions/medium/Search a 2D Matrix.py
@@ -0,0 +1,15 @@
+// Title: Search a 2D Matrix
+            // Difficulty: Medium
+            // Language: Python
+            // Link: https://leetcode.com/problems/search-a-2d-matrix/
+
+            mid = (left + right) // 2
+        left, right = 0, m * n - 1
+
+        while left <= right:
+
+        m, n = len(matrix), len(matrix[0])
+            return False
+        if not matrix or not matrix[0]:
+    def searchMatrix(self, matrix, target):
+class Solution(object):
